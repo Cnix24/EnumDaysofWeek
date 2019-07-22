783181e59d3eb6a4e93aa5a3b5c68421d86a3300
Cnix24 <calvinattl24@gmail.com>
2019-07-22 16:00:21 -0600

    Update

---
+++ DaysOfWeek/DaysOfWeek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DaysOfWeek
+{
+    public enum DayOfWeek
+    {   Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace DaysOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the day of the week.");
+            string input = Console.ReadLine();
+            try
+            {
+                DayOfWeek dayofweek = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), input);
+                Console.WriteLine("The day you have chosen is: " + dayofweek);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
